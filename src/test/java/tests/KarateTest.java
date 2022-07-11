f4295921fdcb7a6c4ac14a94a3477e3a9f0fc39a
taruf <tarufali@hotmail.com>
2022-07-11 20:47:29 +0500

    Initial Cehckin

---
+++ src/test/java/tests/KarateTest.java
@@ -0,0 +1,14 @@
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class TestParallel {
+
+    @Test
+    void testParallel() {
+        Results results = Runner.path("classpath:animals").tags("~@skipme").parallel(5);
+        assertEquals(0, results.getFailCount(), results.getErrorMessages());
+    }
+
+}
