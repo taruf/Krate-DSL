148fc8c780c87d6f4d8e54f93290d98337afa2ca
taruf ali <taruf.ali@backcountry.com>
2022-07-13 13:46:59 +0500

    changes

--- src/test/java/TestParallel.java
+++ src/test/java/TestParallel.java
@@ -1,13 +1,14 @@
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestParallel {
 
     @Test
     void testParallel() {
-        Results results = Runner.path("classpath:animals").tags("~@skipme").parallel(5);
+        Results results = Runner.path("classpath:tests").tags("~@skipme").parallel(1);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
 
--- src/test/java/utils/RandomDataGenerator.java
+++ src/test/java/utils/RandomDataGenerator.java
@@ -1,7 +1,5 @@
 package utils;
 
-import cucumber.api.java.it.Ma;
-
 public class RandomDataGenerator {
     public static int RandomIdGenertor(int lower, int upper) {
         int random_id = (int)Math.floor((Math.random())* (upper-lower+1)+lower);
